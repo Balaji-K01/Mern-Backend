1c85cd4268ce0f9fddc85a24f300357544b72d83
Balaji <balaji01122000@gmail.com>
2026-06-12 12:07:19 +0530

    new files

---
+++ react/react-intro/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Component/Navbar";
+import { Router } from "react-router-dom";
+
+const App=(()=>{
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+})
+
+export default App;
---
+++ react/react-intro/src/Component/Navbar.jsx
@@ -0,0 +1,30 @@
+
+const Navbar=(()=>{
+    return (
+        <>
+            <div className="bg-black p-5">
+                <Logo/>
+            </div>
+        </>
+    )
+})
+
+const Logo=(()=>{
+    return (
+        <>
+        <div className="w-100 mx-2">
+            <img src="../../public/react_logo-removebg-preview.png" alt="logo image" className="w-15"/>
+        </div>
+        </>
+    )
+})
+
+const Link=(()=>{
+    return(
+        <>
+        
+        </>
+    )
+})
+
+export default Navbar;
